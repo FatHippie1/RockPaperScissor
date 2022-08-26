d7df68f25a806d2591111165f7262313bbac032f
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-08-25 19:41:37 -0600

    fixed target.id and consolidated if statements

--- scripts.js
+++ scripts.js
@@ -6,9 +6,15 @@ var playerChoice
 var computerChoice
 var winner
 
-options.forEach(option => option.addEventListener('click', (e) => {
-    playerChoice = e.target.id;
+options.forEach(option => option.addEventListener('click', function (e) {
+    if (e.target !== this) {
+        playerChoice = e.target.parentElement.id;
+    } else {
+        playerChoice = e.target.id;
+    }
+
     displayPlayerPick.innerHTML = playerChoice;
+    console.log('player:' + ' ' + playerChoice);
     generateComputerchoice();
     getResult();
 }));
@@ -28,29 +34,63 @@ function generateComputerchoice() {
     }
 
     displayComputerPick.innerHTML = computerChoice;
+    console.log('computer:' + ' ' + computerChoice);
 }
 
 function getResult() {
     if (computerChoice === playerChoice) {
         winner = 'Tie'
     }
-    if (computerChoice === 'rock' && playerChoice === 'paper') {
+    if ((computerChoice === 'rock' && playerChoice === 'paper') || (computerChoice === 'paper' && playerChoice === 'scissor') || (computerChoice === 'scissor' && playerChoice === 'rock')) {
         winner = 'You Win'
     }
-    if (computerChoice === 'rock' && playerChoice === 'scissor') {
-        winner = 'You Lost'
-    }
-    if (computerChoice === 'paper' && playerChoice === 'scissor') {
-        winner = 'You Win'
-    }
-    if (computerChoice === 'paper' && playerChoice === 'rock') {
-        winner = 'You Lost'
-    }
-    if (computerChoice === 'scissor' && playerChoice === 'rock') {
-        winner = 'You Win'
-    }
-    if (computerChoice === 'scissor' && playerChoice === 'paper') {
+    if ((computerChoice === 'rock' && playerChoice === 'scissor') || (computerChoice === 'paper' && playerChoice === 'rock') || (computerChoice === 'scissor' && playerChoice === 'paper')) {
         winner = 'You Lost'
     }
     displayWinner.innerHTML = winner;
 }
+
+/*
+switch (computerChoice == 'rock') {
+    case playerChoice == 'rock':
+        winner = 'Tie'
+        console.log(winner);
+        break;
+    case playerChoice == 'paper':
+        winner = 'user'
+        console.log(winner);
+        break;
+    case playerChoice == 'scissor':
+        winner = 'computer'
+        console.log(winner);
+        break;
+}
+switch (computerChoice == 'paper') {
+    case playerChoice == 'paper':
+        winner = 'Tie';
+        console.log(winner);
+        break;
+    case playerChoice == 'scissor':
+        winner = 'user';
+        console.log(winner);
+        break;
+    case playerChoice == 'rock':
+        winner = 'computer';
+        console.log(winner);
+        break;
+}
+switch (computerChoice == 'scissor') {
+    case playerChoice == 'scissor':
+        winner = 'Tie';
+        console.log(winner);
+        break;
+    case playerChoice == 'rock':
+        winner = 'user';
+        console.log(winner);
+        break;
+    case playerChoice == 'paper':
+        winner = 'computer';
+        console.log(winner);
+        break;
+}
+*/
