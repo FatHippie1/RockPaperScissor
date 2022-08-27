5850682db446eec34a5494117d4caf9ae3f58f9f
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-08-26 18:57:15 -0600

    added scoreboard

--- scripts.js
+++ scripts.js
@@ -2,9 +2,13 @@ const displayComputerPick = document.getElementById('computer-pick');
 const displayPlayerPick = document.getElementById('player-pick');
 const displayWinner = document.getElementById('winner');
 const options = document.querySelectorAll('button');
-var playerChoice
-var computerChoice
-var winner
+const displayScorePlayer = document.getElementById('scorePlayer');
+const displayScoreComputer = document.getElementById('scoreComputer');
+var playerChoice;
+var computerChoice;
+var winner;
+var computerWin = 0;
+var playerWin = 0;
 
 options.forEach(option => option.addEventListener('click', function (e) {
     if (e.target !== this) {
@@ -41,14 +45,23 @@ function getResult() {
     if (computerChoice === playerChoice) {
         winner = 'Tie'
     }
+
     if ((computerChoice === 'rock' && playerChoice === 'paper') || (computerChoice === 'paper' && playerChoice === 'scissor') || (computerChoice === 'scissor' && playerChoice === 'rock')) {
         winner = 'You Win'
+        playerWin = (playerWin + 1)
     }
+
     if ((computerChoice === 'rock' && playerChoice === 'scissor') || (computerChoice === 'paper' && playerChoice === 'rock') || (computerChoice === 'scissor' && playerChoice === 'paper')) {
         winner = 'You Lost'
+        computerWin = (computerWin + 1)
     }
+
     displayWinner.innerHTML = winner;
+    displayScorePlayer.innerHTML = playerWin;
+    displayScoreComputer.innerHTML = computerWin;
 }
+
+
 
 /*
 switch (computerChoice == 'rock') {
